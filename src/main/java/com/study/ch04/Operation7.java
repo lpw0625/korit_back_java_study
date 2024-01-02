54f008ee3ce8a80c20f1349f80037ab880ffea32
lpw0625 <dlvuddnjsss@gmail.com>
2024-01-02 17:40:25 +0900

    feat:코딩실습변경

--- src/main/java/com/study/ch04/Operation7.java
+++ src/main/java/com/study/ch04/Operation7.java
@@ -3,5 +3,9 @@ package com.study.ch04;
 public class Operation7 {
     public static void main(String[] args) {
         System.out.println("깃,깃허브 수업 실습");
+        System.out.println("깃,깃허브 수업 실습 변경사항 추가");
+        System.out.println("깃,깃허브 수업 실습 변경사항 추가2g");
+
     }
+
 }
