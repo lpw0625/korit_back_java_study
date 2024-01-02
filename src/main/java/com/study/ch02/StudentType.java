83049bdfd1d3db3f5b521bf0f6e58343df9785d9
lpw0625 <dlvuddnjsss@gmail.com>
2024-01-02 16:14:27 +0900

    init: 수업자료최초 커밋

---
+++ src/main/java/com/study/ch02/StudentType.java
@@ -0,0 +1,12 @@
+package com.study.ch02;
+
+public class StudentType extends  Person{ // Person을 extends로 확장을 시켰기에 상속이다.
+    // extends : 확장이라는 의미(상속)
+    // 데이터를 담을 수 있는 범위를 확장시키겠다.
+    // Person이라는 클래스 범위를 Studentype을 담을 정도로 확장시키겠다ㅣ.
+    String address;
+    int grade;
+
+
+
+}
